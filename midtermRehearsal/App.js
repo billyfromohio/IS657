f6b00b06905c4b12c5af948aef15bebf536bd4da
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-09 16:26:30 -0700

    Initial commit of IS657 midterm shizz... need to back up one..:

---
+++ midtermRehearsal/App.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Pressable, TextInput } from 'react-native';
+
+export default function App() {
+  const {number, setNumber} = useState(0);
+  const {message, setMessage} = useState('');
+  
+  function getSquare() {
+    const squared = number * number;
+    return 'The square of ${number} is ${squared}.';
+  }
+
+  function onChangeText(input) {
+    const inputNumber = parseInt(input) || 0;
+    setNumber(inputNumber);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Input a number:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={ number.toString() }
+          keyboardType={"numeric"}
+          />
+          <Button title="Calc Square" onPress={() => setMessage(getSquare())} />
+          <Text>{message}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
+});
