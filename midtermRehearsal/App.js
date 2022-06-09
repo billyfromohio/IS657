b1ff50d306bc3c087c6491eea095f243c03b03fa
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-09 16:47:20 -0700

    i reverted, but who cares

--- midtermRehearsal/App.js
+++ midtermRehearsal/App.js
@@ -4,6 +4,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Pressable, TextInput } from 'react-native';
 
 export default function App() {
+
+
   const [number, setNumber] = useState(0);
   const [message, setMessage] = useState('');
   
