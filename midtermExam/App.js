619abe1552b88376cfeaa75c00612a598a52a2d7
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-09 19:14:57 -0700

    midterm update - setup expo

--- midtermExam/App.js
+++ midtermExam/App.js
@@ -1,10 +1,18 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+// import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <View style={{}}>
+       <Text>IS650 Midterm</Text>
+      </View>
+      <View>
+        <View><Text>{`Number 1 [10 to 20]`}</Text></View>
+        <View><TextInput /></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
