dcfed26657a45f59443959c70da2b183fb8e5fc4
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-10 16:07:59 -0700

    Got the math working. On to validation.

--- midtermPostmortem/App.js
+++ midtermPostmortem/App.js
@@ -4,33 +4,48 @@ import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import { useState } from 'react';
 
 
-
-
 export default function App() {
+
+  const [input1, setNumber1] = useState('');
+  const [input2, setNumber2] = useState('');
+  const [total, setMessage] = useState('');
+
+  function getSum() {
+    const number1 = parseInt(input1);
+    const number2 = parseInt(input2);
+    // if (!number1.isInteger || number2.isInteger || 9 < number1 < 21 || 99 < number2 < 201) {
+    //   setMessage('GTFO');
+    // }else{
+      const sum = number1 + number2;
+      setMessage(sum.toString());
+    // }
+console.log(number2);
+console.log(number2);
+  }
+
+
   return (
     <View style={styles.container}>
      <View style={{flex:1, width: '100%', backgroundColor: 'teal', justifyContent: 'center', margin: 10,}}>
-       <Text style={{fontSize: 30, color: 'gold', alignSelf: 'center'}}>IS657 Midterm</Text>
+       <Text style={{ fontSize: 30, color: 'gold', alignSelf: 'center' }}>IS657 Midterm</Text>
       </View>
       <View style={{flex: 1, width: '100%'}}>
         <View style={styles.inputBoxes}>
           <View style={styles.inputText}><Text>{`Number 1 [10 to 20]`}</Text></View>
-          <View style={styles.inputField}><TextInput style={styles.input} onChangeText1={onChangeText1}  keyboardType={'numeric'} /></View>
+          <View style={styles.inputField}><TextInput style={styles.input} onChangeText={setNumber1}  keyboardType={'numeric'} /></View>
         </View>
         <View style={styles.inputBoxes}>
           <View style={styles.inputText}><Text>{`Number 2 [100 to 200]`}</Text></View>
-          <View style={styles.inputField}><TextInput style={styles.input} onChangeText2={onChangeText2} 
-          keyboardType={'numeric'} /></View>
+          <View style={styles.inputField}><TextInput style={styles.input} onChangeText={setNumber2} keyboardType={'numeric'} /></View>
         </View>
         <View style={styles.inputBoxes}>
           <View style={styles.inputText}>
             <View style={{backgroundColor: 'dodgerblue', width: 130, height: 30, justifyContent: 'center', alignItems: 'center',}}>
-              <Pressable onPress={() => addNumbers(number1,number2)}>
+              <Pressable onPress={(getSum)}>
                 <Text style={{ color: 'white'}}>{`CALCULATE SUM`}</Text>
                 </Pressable></View>
               </View>
-              <View style={styles.inputField}><View style={styles.input}><Text>{addNumbers()}</Text></View></View>
-         
+              <View style={styles.inputField}><View style={styles.input}><Text style={styles.outputText}>{total}</Text></View></View>         
         </View>
       </View>
       <StatusBar style="auto" />
@@ -48,46 +63,57 @@ const styles = StyleSheet.create({
     maxWidth: 350,
     maxHeight: 400,
   },
-  input: {
 
-    height: 30,
-    width: 100,
-    //margin: 12,
-    borderWidth: 1,
-    //justifyContent: 'flex-end',
-    //padding: 10,
-  },
   inputBoxes: {
-   flex: 1,
-   flexDirection: 'row',
-   //alignItems:'center',
-   //justifyContent: 'flex-end',
-//   borderWidth: 1,
-
+    flex: 1,
+    flexDirection: 'row',
 
   },
+
+  // formats the text on left
   inputText: {
     flex: 2,
-  justifyContent: 'center',
-   alignItems: 'flex-start',
-   margin: 5,
-  // borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    margin: 5,
     
   },
+
+  // formats the box arount the input
   inputField: {
     flex: 2,
-  justifyContent: 'center',
-   alignItems: 'flex-end',
-   margin: 5,
-//   borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    margin: 5,
 
   },
+
+    // formats the type in the input fields
+    input: {
+      height: 30,
+      width: 100,
+      justifyContent: 'center',
+      alignItems: 'flex-start', 
+      borderWidth: 1,
+      margin: 5,
+   
+    },
+
+  //formats the box around the output
   outputBox: {
     height: 30,
     width: 100,
-    // alignItems: 'flex-end',
     justifyContent: 'center',
     borderwidth: 1,
+  },
+
+  // formats the output
+  outputText: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    margin: 5,
+  
   },
 
 });
