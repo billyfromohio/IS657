44c46be435a5806373689853b4767fb353fd40a5
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-09 19:31:59 -0700

    30 minute push

--- midtermExam/App.js
+++ midtermExam/App.js
@@ -6,12 +6,22 @@ import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-     <View style={{}}>
+     <View style={{backgroundColor: 'green'}}>
        <Text>IS650 Midterm</Text>
       </View>
       <View>
-        <View><Text>{`Number 1 [10 to 20]`}</Text></View>
-        <View><TextInput /></View>
+        <View style={styles.inputRowBoxes}>
+          <View style={{alignItems:'flex-start'}}><Text>{`Number 1 [10 to 20]`}</Text></View>
+          <View><TextInput style={styles.input}/></View>
+        </View>
+        <View style={styles.inputRowBoxes}>
+        <View style={{alignItems:'flex-start'}}><Text>{`Number 2 [100 to 200]`}</Text></View>
+          <View><TextInput style={styles.input}/></View>
+        </View>
+        <View style={styles.inputRowBoxes}>
+          <View><Text>{`Number 1 [10 to 20]`}</Text></View>
+          <View><TextInput style={styles.input}/></View>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -24,5 +34,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  inputRowBoxes: {
+   flex: 3,
+   flexDirection: 'row',
+
   },
 });
