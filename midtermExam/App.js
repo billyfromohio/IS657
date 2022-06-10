e349b48ee476fbb91a2c18a6d5b9f2ba911e7ca9
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-10 09:51:53 -0700

    friday morning rethink

--- midtermExam/App.js
+++ midtermExam/App.js
@@ -3,32 +3,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import { useState } from 'react';
 
-const [numberText1,  setNumber1] = useState('');
-const [numberText2,  setNumber2] = useState('');
-// const [total, setTotal] = useState(number1 + number2);
 
 
-// function addNumbers() {
-//   const outputSum = number1 + number2;
-//   setTotal(outputSum);
-// }
-function addNumbers () {
+
+export default function App() {
+
+  const [numberText1,  setNumber1] = useState('');
+  const [numberText2,  setNumber2] = useState('');
+
+  const addNumbers = function  () {
   const number1 = parseInt(numberText1);
   const number2 = parseInt(numberText2);
   return number1 + number2
-}
-
-// function onChangeText1(input) {
-//   const number1 = parseInt(input) || 0;
-//   setNumber1(number1);
-// }
-
-// function onChangeText2(input) {
-//   const number2 = parseInt(input) || 0;
-//   setNumber2(number2);
-// }
-
-export default function App() {
+    }
   return (
     <View style={styles.container}>
      <View style={{flex:1, width: '100%', backgroundColor: 'teal', justifyContent: 'center', margin: 10,}}>
@@ -37,7 +24,7 @@ export default function App() {
       <View style={{flex: 1, width: '100%'}}>
         <View style={styles.inputBoxes}>
           <View style={styles.inputText}><Text>{`Number 1 [10 to 20]`}</Text></View>
-          <View style={styles.inputField}><TextInput style={styles.input} onChangeText={setNumber1}  keyboardType={'numeric'} /></View>
+          <View style={styles.inputField}><TextInput style={styles.input} onChangeText={(e) =>setNumber1(e.target.value)}  keyboardType={'numeric'} /></View>
         </View>
         <View style={styles.inputBoxes}>
           <View style={styles.inputText}><Text>{`Number 2 [100 to 200]`}</Text></View>
@@ -50,7 +37,7 @@ export default function App() {
                 <Text style={{ color: 'white'}}>{`CALCULATE SUM`}</Text>
                 </Pressable>
               </View>
-              <View style={styles.inputField}><View style={styles.input}><Text>{'total'}</Text></View></View>
+              <View style={styles.inputField}><View style={styles.input}><Text>{total}</Text></View></View>
           </View>
         </View>
       </View>
@@ -58,7 +45,7 @@ export default function App() {
     </View>
 
   );
-console.log(addedUp);
+
 }
 
 const styles = StyleSheet.create({
