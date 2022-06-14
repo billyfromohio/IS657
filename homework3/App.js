d9cdb9f9d1c2abb886b8ba65fdd368a62c170d9a
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-14 16:59:14 -0700

    Really-ish working... lest see

--- homework3/App.js
+++ homework3/App.js
@@ -1,37 +1,51 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+
 import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useFonts } from 'expo-font';
+import { AppLoading }  from 'expo-splash-screen';
 import { ImageBackground } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-
+  const [loaded] = useFonts({
+    MouseMemoirs: require('./assets/fonts/MouseMemoirs-Regular.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
+    
   return (
     
     
     <NavigationContainer>
       <Tab.Navigator
       screenOptions={{
-        headerShown: false,
         tabBarInactiveTintColor: 'green',
         tabBarActiveTintColor: 'blueviolet',
         tabBarStyle: {
           fontSize: 30,
           height: 50,
-          backgroundColor: 'black', } 
-        
+          backgroundColor: 'black', 
+          color: 'black',
+        } 
       }}>
         <Tab.Screen
           name="Wizard"
           component={MainScreen}
+         options={{ headerShown: false, }} 
         />
         <Tab.Screen
         name="Magic"
         component={MagicScreen}
-        initialParams={{}} />
+        initialParams={{}}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
@@ -40,7 +54,7 @@ export default function App() {
 function MainScreen({ navigation }) {
   return (
     <ImageBackground  style={{height:'100%', width:'100%'}}  source={ require('./assets/wizardImg.png' ) }>
-      <View style={styles.container}>
+      <View style={styles.wizardContainer}>
         <Pressable onPress={() => navigation.navigate('Magic', )}>
           <Text style={styles.textStyle}>So you want to be a wizard?</Text>
         </Pressable>
@@ -53,13 +67,13 @@ function MainScreen({ navigation }) {
 function MagicScreen({ navigation, route }) {
   return (
     
-    <View style={styles.magicContainer}>
+    <View style={styles.container}>
       
      <Pressable onPress={() => navigation.navigate('Wizard', { name: 'Me' })}>
       <Text style={styles.magicTextStyle}>Oh, that's too bad.</Text>
       <Text style={styles.magicTextStyle}>{'\t'}You see, there's really no such
       thing as magic.</Text><Text style={styles.magicTextStyle}>{'\t'} It's really just a combination of distraction, illusion and a whole lot of 
-      wishful thinking.</Text><Text style={styles.magicTextStyle}>{'\t'} But, it is still fun!</Text>
+      wishful thinking.</Text><Text style={styles.magicTextStyle}> But, it is still fun!</Text>
       </Pressable>
     </View>
   );
@@ -71,13 +85,13 @@ function MagicScreen({ navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //backgroundColor: '#000',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  magicContainer: {
+  wizardContainer: {
     flex: 1,
-    backgroundColor: '#000',
+   // backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
