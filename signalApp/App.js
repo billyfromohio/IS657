be3dbde1fb3782e8c9fc989d645e6160bb452d55
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-15 23:07:55 -0700

    signalApp initial commit

---
+++ signalApp/App.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
+import 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
