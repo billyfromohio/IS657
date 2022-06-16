c503e2d2a633f90337a7d80960c562b758e3c591
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-15 23:49:43 -0700

    last commit 6/15

--- signalApp/App.js
+++ signalApp/App.js
@@ -1,18 +1,23 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'; 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen.js';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen.js";
 
 const Stack = createNativeStackNavigator();
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: 'white',
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Login' component={LoginScreen} />
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -21,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- signalApp/screens/LoginScreen.js
+++ signalApp/screens/LoginScreen.js
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Button, Input, Image} from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View> 
+      <StatusBar style="light" />
+      <Image source={{
+        uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+      }}
+      style={{ width: 200, height: 200 }}/>
     </View>
   )
 }
