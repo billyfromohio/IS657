83048299db1be1ada24bf78dcd0c326b40ee9f10
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-18 17:03:14 -0700

    add chatlistitem

--- chatDemo/ChatListItem.js
+++ chatDemo/ChatListItem.js
@@ -13,17 +13,46 @@ import {
 } from 'firebase/firestore';
 import firebaseApp from './firebase';
 
+const db = getFirestore(firebaseApp);
 
 const ChatListItem = ({ id, chatName, enterChat }) => {
+    const [message, setMessage] = useState(null);
 
-    
+    useEffect(() => {
+        const messagesRef = collection(db, 'chats', id, 'messages');
+        const q = query(messageRef, orderBy('timestamp', 'desc'), limit(1));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                setMessage(doc.data());
+            });  
+        });
+        return unsubscribe;
+    }, []);
+
+
   return (
-    <View>
-      <Text>ChatListItem</Text>
-    </View>
+    <ListItem onPress={() => enterChat( id, chatName)}>
+        <Avatar
+        source={{
+            uri: 'https://www.gstatic.com/mobilesdk/180227_mobilesdk/database_rules_zerostate.png',
+        }}
+        />
+        <ListItem.Content>
+            <ListItem.Title style={styles.title}>{chatName}</ListItem.Title>
+            <ListItem.SubTitle numberOfLines={1} ellipsizeMode='tail'>
+                {message
+                ? message.displayName + ' : ' + message.messages
+                : 'No Message'}
+            </ListItem.SubTitle>
+        </ListItem.Content>
+    </ListItem>    
   )
 }
 
 export default ChatListItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    title:{
+        fontWeight: '800',
+    },
+});
