cfad6642f0dbb844cb2bfc2932b4961e4d604225
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-23 14:44:08 -0700

    Added SafeArea

--- instaCloneFinal/App.js
+++ instaCloneFinal/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from './components/auth/Landing';
 import Register from './components/Register';
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import firebaseApp from './firebase';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const auth = getAuth(firebaseApp);
 onAuthStateChanged(auth, user => {
@@ -18,6 +19,8 @@ onAuthStateChanged(auth, user => {
 const Stack = createStackNavigator();
 export default function App() {
   return (
+    <SafeAreaProvider>
+      <SafeAreaView  style={{ flex: 1, }}>
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Landing'>
         <Stack.Screen name='Landing' component={Landing} options={{ headerShown: false}} />
@@ -25,5 +28,7 @@ export default function App() {
 
       </Stack.Navigator>
     </NavigationContainer>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
