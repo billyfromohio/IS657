6e3aa1c216ab74721ad3fe968c3692a7c57c8301
Billy P <84866949+billyfromohio@users.noreply.github.com>
2022-06-22 21:25:18 -0700

    Added Firebase Config

--- instaCloneFinal/App.js
+++ instaCloneFinal/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import * as firebase from "firebase";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from './components/auth/Landing';
+import firebaseConfig from './config';
 
 const Stack = createStackNavigator();
 export default function App() {
---
+++ instaCloneFinal/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDvVJgeLcIKGvIqgSYoOcGbu4otfyIUJw",
+    authDomain: "instaclonefinalbfo.firebaseapp.com",
+    projectId: "instaclonefinalbfo",
+    storageBucket: "instaclonefinalbfo.appspot.com",
+    messagingSenderId: "614726814757",
+    appId: "1:614726814757:web:9838fec67896f2c61703cc"
+  };
+
+  export default firebaseConfig;
